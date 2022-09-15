7125b0ce409768f0de4afd6dbeab3bd78df4cb8d
harshdubey11 <harsh.110901@gmail.com>
2022-09-15 23:46:12 +0530

    initial commit

--- src/components/events/data.js
+++ src/components/events/data.js
@@ -1,223 +1,194 @@
 const data=[
-    {
-      id:1,
-      title:"Web Challenge",
-      poster:"images/web.png",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  {
+    id:1,
+    title:"Web Challenge",
+    poster:"images/web.png",
+    incharge:["1:Sonu Kumar","2:"],
+    volunteers:["1.Rishab Mishra(CSE 4th Year)",
+    "2.Shorya Chaturvedi(CSE 4th Year)",
+    "3.Ajeet Kumar(CSE 4th Year)",
+    "4.Anshu Mishra(IT 3rd Year)",
+    "5.Ishita Yadav (IT 3rd Year)"],
+    faculty:["1. Ms. Sana Afreen",
+            "2. Ms. Sarika Singh"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:8957896305"
 
-    },
-    {
-      id:2,
-      title:"Treasure Hunt",
-      poster:"images/tressure.jpg",
-      incharge:["1:Shivansh Khare","2:"],
-      volunteers:["1.Akash Singh (CSE 4th Year)",
-      "2.Abu Bakar (CSE 4th Year)",
-      "3.Sahil Kumar (CSE 4th Year)",
-      "4.Navya Yadav (CSE 4th Year)",
-      "5.Rajat (CSE 3rd Year)",],
-      faculty:["1.Archana Dwivedi",
-      "2.Saumya Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:""
+  },
+  {
+    id:2,
+    title:"Treasure Hunt",
+    poster:"images/tressure.jpg",
+    incharge:["1:Shivansh Khare","2:"],
+    volunteers:["1.Akash Singh (CSE 4th Year)",
+    "2.Abu Bakar (CSE 4th Year)",
+    "3.Sahil Kumar (CSE 4th Year)",
+    "4.Deepankan Srivastava (IT 3rd Year)",
+    "5.Rajat Srivastava (CSE 3rd Year)",],
+    faculty:["1. Ms. Archana Dwivedi",
+    "2. Ms. Saumya Singh"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:8887988958"
 
-    },
-    {
-      id:3,
-      title:"Mock Interview",
-      poster:"images/mock interview.jpg",
-      incharge:["1:Jaya Khare","2:"],
-      volunteers:["1.Pavitra Dwivedi (CSE 4th Year)",
-      "2.Monika Patel (CSE 4th Year)",
-      "3.Tushika (CSE 3rd Year)"],
-      faculty:["1.Apporva Mishra"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:""
+  },
+  {
+    id:3,
+    title:"Mock Interview",
+    poster:"images/mock interview.jpg",
+    incharge:["1:Jaya Khare","2:"],
+    volunteers:[
+    "1.Swapnil Verma (IT 3rd Year)",
+    "2.Tushika Srivastava (CSE 3rd Year)"],
+    faculty:["1. Mrs. Apporva Mishra", "2. Ms. Arpita Srivastava"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:6392004353"
 
-    },
-    {
-      id:4,
-      title:"Coding",
-      poster:"images/coding.jpg",
-      incharge:["1:Harsh Dubey","2:"],
-      volunteers:["1.Alok Pal (CSE 4th Year)",
-        "2.Vishwadev Pratap Singh (CSE 4th Year)",
-        "3.Sudeep Jaiswal (CSE 4th Year)",
-        "4.Manoj Pandey (CSE 4th Year)"],
-      faculty:["1.Faizan Mahmood",
-              "2.Akanksha Srivastava"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:""
+  },
+  {
+    id:4,
+    title:"Coding Contest",
+    poster:"images/coding.jpg",
+    incharge:["1:Harsh Dubey","2:"],
+    volunteers:["1.Alok Pal (CSE 4th Year)",
+      "2.Vishwadev Pratap Singh (CSE 4th Year)",
+      "3.Sudeep Jaiswal (CSE 4th Year)",
+      "4.Manoj Pandey (CSE 3rd Year)",
+      "5. Ankit Kumar Maurya (IT 3rd Year)"],
+    faculty:["1.Faizan Mahmood",
+            "2.Akanksha Srivastava"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:9517229130"
 
-    },
-    {
-      id:5,
-      title:"Technical Quiz",
-      poster:"images/tech quiz.jpg",
-      incharge:["1:Ujjwal Srivastava","2:"],
-      volunteers:["1.Gauri Mishra (CSE 4th Year)",
-       " 2.Kartikey Singh (CSE 4th Year)",
-        "3.Devarishi (CSE 3rd Year)",
-        "4.Om Khare (CSE 3rd Year)"],
-      faculty:["1.Nirupama Pathak",
-        "2.Deepti Razdan"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:""
+  },
+  {
+    id:5,
+    title:"Reasoning & Technical Quiz",
+    poster:"images/tech quiz.jpg",
+    incharge:["1:Nisha Kumari","2:"],
+    volunteers:["1.Sneha Singh Pal (CSE 4th Year)",
+     " 2.Navya Yadav (CSE 4th Year)",
+      "3.Ujjwal Srivastava (CSE 4th Year)",
+      "4.Shaswat Chauhan (CSE 3rd Year)",
+      "5.Sujeet Kumar Sahu (IT 3rd Year)"],
+    faculty:["1.Ashish Mishra",
+      "2.Nirupma Pathak"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:7979962585"
 
-    },
-    {
-      id:6,
-      title:"Nexxplora Shine",
-      poster:"images/shine.jpg",
-      incharge:["1:Mansi jain","2:"],
-      volunteers:["1.Harsh Vardhan Singh (CSE 4th Year)",
-       " 2.Pawan Kumar (CSE 4th Year)",
-       " 3.Aryanshi Singh (CSE 3rd Year)"],
-      faculty:["1.Ram Dubey",
-        "2.Anurag Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:""
+  },
+  {
+    id:6,
+    title:"Nexxplora Shine",
+    poster:"images/shine.jpg",
+    incharge:["1:Mansi jain","2:"],
+    volunteers:["1.Harsh Vardhan Singh (CSE 4th Year)",
+     " 2.Monika Patel (CSE 4th Year)",
+     " 3.Gauri Mishra (CSE 4th Year)",
+     " 4.Aryanshi Singh (CSE 3rd Year)",
+      " 5.Sandhya Maddhesiya (IT 3rd Year)"],
+    faculty:["1.Ram Dubey",
+      "2.Anurag Singh"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:9557020393"
 
-    },
-    {
-      id:7,
-      title:"Arm wrestling",
-      poster:"images/arm.jpg",
-      incharge:["1:Akhilendra Mishra","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  },
+  {
+    id:7,
+    title:"Arm wrestling",
+    poster:"images/arm.jpg",
+    incharge:["1:Akhilendra Mishra","2:"],
+    volunteers:["1.Udit Srivastava (CSE 4th Year)",
+    "2.Mukul Singh (CSE 3rd Year)"],
+    faculty:["1.Sumit Yadav",
+            "2.Anoop Mathur"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:6386068129"
 
-    },
-    {
-      id:8,
-      title:"BGMI",
-      poster:"images/bgmi.jpg",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  },
+  {
+    id:8,
+    title:"BGMI",
+    poster:"images/bgmi.jpg",
+    incharge:["1:Rishab Singh","2:"],
+    volunteers:["1.Shivendra Srivastava(CSE 4th Year)",
+    "2.Harsh Dubey (IT 3rd Year)"],
+    faculty:["1.Sumit Yadav",
+            "2.Anoop Mathur"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:6393411553"
 
-    },
-    {
-      id:9,
-      title:"Need For Speed",
-      poster:"images/NFSS.jpg",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  },
+  {
+    id:9,
+    title:"Need For Speed",
+    poster:"images/NFSS.jpg",
+    incharge:["1:Ajay Tandon","2:"],
+    volunteers:["1.Pawan Kumar Singh (CSE 4th Year)",
+    "2.Shubham Shukla (CSE 3rd Year)"],
+   faculty:["1.Sumit Yadav",
+            "2.Anoop Mathur"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:7651893649"
 
-    },
-    {
-      id:10,
-      title:"Poster making",
-      poster:"images/poster making.jpg",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  },
+  {
+    id:10,
+    title:"Poster making",
+    poster:"images/poster making.jpg",
+    incharge:["1:Farhan","2:"],
+    volunteers:[
+    "1.Harsh Mishra(IT 3rd Year)",
+    "2.Pragya Singh (CSE 3rd Year)"],
+    faculty:["1.Dushyant Srivastava",
+            "2.Deepti Rajdan"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:6392182441"
 
-    },
-    {
-      id:11,
-      title:"Project Presentation",
-      poster:"images/presentation.jpeg",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  },
+  {
+    id:11,
+    title:"Project Presentation",
+    poster:"images/presentation.jpeg",
+    incharge:["1:Bhumika Srivastava","2:"],
+    volunteers:[
+    "1.Km. Anshula (CSE 4th Year)",
+    "2.Gopal Tiwari(IT 3rd Year)",
+    "3.Harsh Barnawal (CSE 3rd Year)"],
+    faculty:["1.Mr. Saif Zaheer"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:8795438658"
 
-    },
-    {
-      id:12,
-      title:"Extempore",
-      poster:"images/extempore.png",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  },
+  {
+    id:12,
+    title:"Extempore",
+    poster:"images/extempore.png",
+    incharge:["1:Babita Pandey","2:"],
+    volunteers:[
+    "1.Deepa Sen Nirala (IT 3rd Year)",
+    "2.Poornima Singh (CSE 3rd Year)"],
+      faculty:["1. Mrs. Apporva Mishra", "2. Ms. Arpita Srivastava"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:9125958225"
 
-    },
-    {
-      id:13,
-      title:"Reasoning and Aptitude",
-      poster:"images/aptitude.png",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
+  },
+  {
+    id:13,
+    title:"Placed",
+    poster:"images/placenewww.jpg",
+    incharge:["1:Prakhar Saxena","2:Neelesh Prajapati"],
+    volunteers:[
+    "1.Devansh Singh(IT 3rd Year)",
+    "2.Shweta Yadav (CSE 3rd Year)"],
+    faculty:["1.Sambhav Agarwal",
+            "2.Rahul Gupta",
+            "3.Dushyant Srivastava",
+            "4.Sanjay Tandon"],
+      winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
+      phone:"tel:8954535590"
 
-    },
-    {
-      id:14,
-      title:"Placed",
-      poster:"images/placenewww.jpg",
-      incharge:["1:Sonu Kumar","2:"],
-      volunteers:["1.Rishab Mishra(CSE 4th Year)",
-      "2.Shorya Chaturvedi(CSE 4th Year)",
-      "3.Ajeet Kumar(CSE 4th Year)",
-      "4.Anshu Mishra(IT 3rd Year)",
-      "5.Saumya (CSE 3rd Year)"],
-      faculty:["1.Sana Afreen",
-              "2.Sarika Singh"],
-        winners:["1.(Yet To Announce)","1.(Yet To Announce)","1.(Yet To Announce)"],
-        phone:"tel:8957896305"
-
-    },
+  },
 
 ];
 
 export default data;
-
--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -7,6 +7,11 @@ export default function Events(){
     return (
        
         <div className="events" id="events">
+        <div className='btn'>
+            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdtfhKUUuPEqTRlON0WjMt_0I513avg_fxYHcKZvSM1-DfwKw/viewform">
+            <button> Click To Register</button>
+            </a>
+        </div>
         <div className='head'>Events</div>
         <div className='cards'>
         {data.map(item =>{
